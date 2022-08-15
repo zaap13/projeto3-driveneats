aad5b98497c9e10cf33709a53c9235d97b8f84f6
Felipe Bueno <felipebueno2201@gmail.com>
2022-08-15 16:57:43 -0300

    FinalDetalhado

--- script.js
+++ script.js
@@ -3,89 +3,89 @@ const bebida = new Array(2);
 const sobremesa = new Array(2);
 const clientData = new Array(3);
 
-const ativar = document.querySelector('.button')
+const ativar = document.querySelector('.button');
 ativar.disabled = true;
 
 function ativaBotao() {
     if (prato[0] !== undefined && bebida[0] !== undefined && sobremesa[0] !== undefined) {
         ativar.disabled = false;
-        ativar.innerHTML = "Fechar Pedido"
-        ativar.classList.add("ativo")
+        ativar.innerHTML = "Fechar Pedido";
+        ativar.classList.add("ativo");
     }
 }
 
 function selecionarPrato(classePrato) {
-    const pratoSelecionado = document.querySelector(".prato .selecionado")
+    const pratoSelecionado = document.querySelector(".prato .selecionado");
 
     if (pratoSelecionado !== null) {
-        const checking = document.querySelector(".selecionado .check")
-        checking.classList.add("hidden")
-        pratoSelecionado.classList.remove("selecionado")
+        const checking = document.querySelector(".selecionado .check");
+        checking.classList.add("hidden");
+        pratoSelecionado.classList.remove("selecionado");
     }
 
-    const botao = document.querySelector(classePrato)
-    botao.classList.add("selecionado")
+    const botao = document.querySelector(classePrato);
+    botao.classList.add("selecionado");
 
-    const checking = document.querySelector(`${classePrato} .check`)
+    const checking = document.querySelector(`${classePrato} .check`);
     if (checking !== null) {
-        checking.classList.remove("hidden")
+        checking.classList.remove("hidden");
     }
 
-    const nomePrato = document.querySelector(`${classePrato} .nome-prato`)
+    const nomePrato = document.querySelector(`${classePrato} .nome-prato`);
     prato[0] = nomePrato.innerHTML;
-    const valorPrato = document.querySelector(`${classePrato} .valor-prato`)
+    const valorPrato = document.querySelector(`${classePrato} .valor-prato`);
     prato[1] = valorPrato.innerHTML;
 
     ativaBotao();
 }
 
 function selecionarBebida(classeBebida) {
-    const bebidaSelecionada = document.querySelector(".bebida .selecionado")
+    const bebidaSelecionada = document.querySelector(".bebida .selecionado");
 
     if (bebidaSelecionada !== null) {
-        const checking = document.querySelector(".bebida .selecionado .check")
-        checking.classList.add("hidden")
-        bebidaSelecionada.classList.remove("selecionado")
+        const checking = document.querySelector(".bebida .selecionado .check");
+        checking.classList.add("hidden");
+        bebidaSelecionada.classList.remove("selecionado");
     }
 
-    const botao = document.querySelector(classeBebida)
-    botao.classList.add("selecionado")
+    const botao = document.querySelector(classeBebida);
+    botao.classList.add("selecionado");
 
-    const checking = document.querySelector(`${classeBebida} .check`)
+    const checking = document.querySelector(`${classeBebida} .check`);
     if (checking !== null) {
-        checking.classList.remove("hidden")
+        checking.classList.remove("hidden");
     }
 
-    const nomeBebida = document.querySelector(`${classeBebida} .nome-bebida`)
+    const nomeBebida = document.querySelector(`${classeBebida} .nome-bebida`);
     bebida[0] = nomeBebida.innerHTML;
-    const valorBebida = document.querySelector(`${classeBebida} .valor-bebida`)
+    const valorBebida = document.querySelector(`${classeBebida} .valor-bebida`);
     bebida[1] = valorBebida.innerHTML;
 
     ativaBotao();
 }
 
 function selecionarSobremesa(classeSobremesa) {
-    const sobremesaSelecionada = document.querySelector(".sobremesa .selecionado")
+    const sobremesaSelecionada = document.querySelector(".sobremesa .selecionado");
 
     if (sobremesaSelecionada !== null) {
-        const checking = document.querySelector(".sobremesa .selecionado .check")
-        checking.classList.add("hidden")
-        sobremesaSelecionada.classList.remove("selecionado")
+        const checking = document.querySelector(".sobremesa .selecionado .check");
+        checking.classList.add("hidden");
+        sobremesaSelecionada.classList.remove("selecionado");
     }
 
-    const botao = document.querySelector(classeSobremesa)
-    botao.classList.add("selecionado")
+    const botao = document.querySelector(classeSobremesa);
+    botao.classList.add("selecionado");
 
 
 
-    const checking = document.querySelector(`${classeSobremesa} .check`)
+    const checking = document.querySelector(`${classeSobremesa} .check`);
     if (checking !== null) {
-        checking.classList.remove("hidden")
+        checking.classList.remove("hidden");
     }
 
-    const nomeSobremesa = document.querySelector(`${classeSobremesa} .nome-sobremesa`)
+    const nomeSobremesa = document.querySelector(`${classeSobremesa} .nome-sobremesa`);
     sobremesa[0] = nomeSobremesa.innerHTML;
-    const valorSobremesa = document.querySelector(`${classeSobremesa} .valor-sobremesa`)
+    const valorSobremesa = document.querySelector(`${classeSobremesa} .valor-sobremesa`);
     sobremesa[1] = valorSobremesa.innerHTML;
 
     ativaBotao();
@@ -123,11 +123,11 @@ function confirmaWpp() {
 Total: R$ ${(clientData[2].toFixed(2)).replace('.', ',')}
 
 Nome: ${clientData[0]}
-Endereço: ${clientData[1]}`
+Endereço: ${clientData[1]}`;
 
     let msgWpp = encodeURIComponent(wppMsg);
 
-    window.open(`https://wa.me/5543984930900?text=${msgWpp}`)
+    window.open(`https://wa.me/5543984930900?text=${msgWpp}`);
 }
 
 function cancelaPedido(){
